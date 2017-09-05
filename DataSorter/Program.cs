e38332757e154f98b461eef355a4b672a4f55de0
Chris Saylor <csaylor@jmttg.com>
2017-09-05 08:03:29 -0600

    bears

--- DataSorter/Program.cs
+++ DataSorter/Program.cs
@@ -3,13 +3,125 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
 
 namespace DataSorter
 {
     class Program
     {
+        public const string AppName = "Sheets Test";
+        public const string SheetId = "1d35X8k_0TZ8q02WwLHlEViV4Nb6ZiJ4VS9YiDzNFIXc";
+        public static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        //sheets api key
+        //AIzaSyB0XWVcuOIhYPVw4miF1FT6TdssHt7Xr7o
+
+        //oauth client id
+        //697751519570-htsksih7595i6hle45o41objmt3qitkr.apps.googleusercontent.com
+        //oauth client secret
+        //AiAEHMuXO277YtnyKPjhDOsd
+
+        private static List<DataModel> ReadGoogle()
+        {
+            UserCredential cred = null;
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-sheets-test.json");
+                cred = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = cred,
+                ApplicationName = AppName,
+            });
+
+            SpreadsheetsResource.ValuesResource.GetRequest req = service.Spreadsheets.Values.Get(SheetId, "Individual MOC List!A2:O435");
+    
+            var resp = req.Execute();
+            if (resp.Values != null && resp.Values.Count > 0)
+            {
+                foreach(var row in resp.Values)
+                {
+
+                }
+            }
+            return null;
+        }
         static void Main(string[] args)
         {
+
+            var bears = ReadGoogle();
+
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "\\schedule.csv";
+            var outPath = AppDomain.CurrentDomain.BaseDirectory + "\\Schedule.html";
+            if (File.Exists(outPath)) File.Delete(outPath);
+            char delimiter = '|';
+
+            var dataList = new List<DataModel>();
+            var reader = File.OpenText(filePath);
+            var line = reader.ReadLine();
+            bool isFirst = true;
+            while (line != null)
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                }
+                else
+                {
+                    dataList.Add(new DataModel(line.Split(delimiter)));
+                }
+
+                line = reader.ReadLine();
+            }
+            dataList.RemoveAll(x => string.IsNullOrEmpty(x.CreditUnionName));
+            var sb = new StringBuilder(WebPieces.Start);
+            sb.AppendLine(WebPieces.CSS);
+
+
+            foreach (var stateGroup in dataList.GroupBy(x => x.State).OrderBy(x => x.Key))
+            {
+                sb.AppendLine("<h2 class=\"underline\">" + stateGroup.Key + "</h2>");
+                sb.AppendLine("<div class=\"break major\"></div>");
+                foreach (var congressGroup in stateGroup.GroupBy(x => x.Congressman).OrderBy(x => x.Key))
+                {
+                    foreach (var dateGroup in congressGroup.GroupBy(x => new { x.CombinedDate, x.Location }).OrderBy(x => x.Key.CombinedDate))
+                    {
+                        sb.AppendLine("<div class=\"indent-left\">");
+                        sb.AppendLine("<div class=\"width-100\">");
+                        sb.AppendLine("<span class=\"span-block italics larger\">" + congressGroup.Key + "</span>");
+                        sb.AppendLine("<span class=\"span-block indent\">DAY OF WEEK" + "," + dateGroup.Key.CombinedDate.Date.ToShortDateString() + "</span>");
+                        sb.AppendLine("<span class=\"span-block indent\">" + dateGroup.Key.CombinedDate.ToShortTimeString() + "</span>");
+                        sb.AppendLine("<span class=\"span-block indent\">" + dateGroup.Key.Location + "</span>");
+                        sb.AppendLine("</div>");
+                        sb.AppendLine("<ul>");
+                        foreach (var fcu in dateGroup.GroupBy(x => x.CreditUnionName).OrderBy(x => x.Key))
+                        {
+                            sb.AppendLine("<li>" + fcu.Key + "</li>");
+                        }
+                        sb.AppendLine("</ul>");
+                        sb.AppendLine("<div class=\"break minor\"></div>");
+                        sb.AppendLine("</div>");
+                    }
+
+                }
+            }
+
+            sb.Append(WebPieces.End);
+            var writer = File.CreateText(outPath);
+            writer.WriteLine(sb.ToString());
+            writer.Close();
+            System.Diagnostics.Process.Start(outPath);
         }
     }
 }
---
+++ DataSorter/WebPieces.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSorter
+{
+    public class WebPieces
+    {
+        public const string CongressBlock = @"<div class=""indent-left"">
+        <div class=""width-100"">
+            <span class=""span-block italics larger"">[CongressName]</span>
+            <span class=""span-block indent"">[MeetingDay], [MeetingDate]</span>
+            <span class=""span-block indent"">[MeetingTime]</span>
+            <span class=""span-block indent"">[Location]</span>
+        </div>
+        <ul>
+[FCUBlock]
+        </ul>
+        <div class=""break minor""></div>
+    </div>";
+
+        public const string StateBlock = @"    <h2 class=""underline"">[StateName]</h2>
+    <div class=""break major""></div>
+[CongressBlock]";
+        public const string FCUBlock = @"<li class=""bold"">[FCUName]</li>";
+
+
+        public const string End = @"</body>
+</html>";
+        public const string Start = @"<!DOCTYPE html>
+<html lang=""en"" xmlns=""http://www.w3.org/1999/xhtml"">
+<head>
+    <meta charset=""utf-8"" />
+    <title>Meeting Schedule</title>
+</head>
+<body>";
+        public const string CSS = @"    <style type=""text/css"">
+        body {
+            font-family: ""Arial"";
+        }
+
+        .indent-left {
+            padding-left: 50px;
+            float: left;
+            width: 100%;
+        }
+
+        li, ul {
+            padding-top: 3px;
+        }
+
+        .larger {
+            font-size: 1.4em;
+        }
+
+        .width-100 {
+            width: 100%;
+        }
+
+        div {
+            float: left;
+        }
+
+        .left {
+            float: left;
+        }
+
+        ul li ul li {
+            font-weight: normal;
+        }
+
+        .bold {
+            font-weight: bolder;
+        }
+
+        .break {
+            float: left;
+            width: 90%;
+            margin-bottom: 5px;
+        }
+
+            .break.major {
+                background-color: blue;
+                height: 3px;
+            }
+
+            .break.minor {
+                background-color: gray;
+                height: 2px;
+            }
+
+        .italics {
+            font-style: italic;
+        }
+
+        .underline {
+            text-decoration: underline;
+        }
+
+        .span-block {
+            display: inline-block;
+            font-weight: bold;
+            font-size: 1.1em;
+        }
+
+            .span-block.indent {
+                margin-left: 20px;
+            }
+
+        ul {
+            margin-left: 100px;
+        }
+    </style>";
+    }
+}
